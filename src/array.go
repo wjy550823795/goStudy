b3aa0f388055a53932245c95f64c2d60fa56be98
jy.wang <jy.wang@zuche.com>
2024-02-28 14:20:00 +0800

    数组真拉

--- src/array.go
+++ src/array.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 	//固定长度
-	var myArray1 [10]string
+	var myArray1 [10]int
 	//有初始值
 	myArray2 := [10]int{1, 2, 3, 4}
 	//长度不同 类型也不同
-	myArray3 := [4]int{1, 2, 3, 4}
+	myArray3 := [4]int{10, 20, 30, 40}
 	for i := 0; i < len(myArray1); i++ {
 		fmt.Println(myArray1[i])
 	}
@@ -21,10 +21,15 @@ func main() {
 	fmt.Printf("myArray3 types = %T\n", myArray3)
 
 	printArray(myArray3)
+	fmt.Println("==========")
+	for index, val := range myArray3 {
+		fmt.Println("index = ", index, "val = ", val)
+	}
 }
 
 func printArray(myArray [4]int) {
 	for index, val := range myArray {
 		fmt.Println("index = ", index, "val = ", val)
 	}
+	myArray[0] = 100
 }
