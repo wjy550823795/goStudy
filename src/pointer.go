e87c127ecb9651d150ce83503400d1c0e998901d
jy.wang <jy.wang@zuche.com>
2024-02-28 10:47:55 +0800

    指针done

--- src/pointer.go
+++ src/pointer.go
@@ -5,16 +5,33 @@ import "fmt"
 func changeValue(p int) {
 	p = 10
 }
+func changeValue2(p *int) {
+	*p = 10
+}
 func main() {
 	//var a int = 1
 	//changeValue(a)
 	//fmt.Print("a=", a) //结果为1
 
-	var a int = 1
-	changeValue2(&a)
-	fmt.Print("a=", a) //结果为10
+	//var a int = 1
+	//changeValue2(&a)
+	//fmt.Print("a=", a) //结果为10
+
+	var a int = 10
+	var b int = 20
+	//swap(a, b)
+	swap2(&a, &b)
+	fmt.Println("a=", a, "b=", b)
 }
 
-func changeValue2(p *int) {
-	*p = 10
+func swap(a int, b int) {
+	var temp int = a
+	a = b
+	b = temp
+}
+
+func swap2(a *int, b *int) {
+	var temp int = *a
+	*a = *b
+	*b = temp
 }
