73e4944b7822f6e8c3865cf754658c5f7c794e80
jy.wang <jy.wang@zuche.com>
2024-02-29 18:21:18 +0800

    类的继承

---
+++ src/structExtend.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	name string
+	sex  string
+}
+
+func (this *Human) Eat() {
+	fmt.Println("Human.Eat()...")
+}
+func (this *Human) Walk() {
+	fmt.Println("Human.Walk()...")
+}
+
+/*==============================================*/
+// 子类
+type SuperMan struct {
+	Human
+	level int
+}
+
+// 子类重写
+func (this *SuperMan) Eat() {
+	fmt.Println("SuperMan.Eat()...")
+}
+
+// 子类的新方法
+func (this *SuperMan) Flow() {
+	fmt.Println("SuperMan.Flow()...")
+}
+func (this SuperMan) print() {
+	fmt.Println("superMan.name=", this.name)
+	fmt.Println("superMan.sex=", this.sex)
+	fmt.Println("superMan.level=", this.level)
+}
+
+func main() {
+	h := Human{"zhang3", "haha"}
+	h.Eat()
+	h.Walk()
+	fmt.Println("===================")
+
+	//s := SuperMan{Human{"zhangsan", "男"}, 100}
+	//两种定义方式
+	var s SuperMan
+	s.name = "li4"
+	s.sex = "女"
+	s.level = 99
+	s.Walk() //父类方法
+	s.Eat()  //子类重写方法
+	s.Flow() //子类的新方法
+	s.print()
+
+}
