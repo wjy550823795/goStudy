9d21c71ad7c42f06984f7a5125a619e8b7e62a22
jy.wang <jy.wang@zuche.com>
2024-02-29 10:14:23 +0800

    mao声明

--- src/map.go
+++ src/map.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//声明map类型
+	var myMap map[string]string
+	if myMap == nil {
+		fmt.Println("myMap 是empty")
+	}
+	//需要先发分配空间
+	myMap = make(map[string]string, 10)
+	myMap["one"] = "java"
+	myMap["two"] = "c++"
+	myMap["three"] = "golang"
+
+	fmt.Println(myMap) //乱序
+
+	/*=====================*/
+	myMap2 := make(map[int]string)
+	myMap2[1] = "java"
+	myMap2[2] = "c++"
+	myMap2[3] = "golang"
+	fmt.Println(myMap2) //乱序
+
+	/*======================*/
+	myMap3 := map[string]string{
+		"one":   "java",
+		"two":   "c++",
+		"trhee": "golang",
+	}
+	fmt.Println(myMap3) //乱序
 }
