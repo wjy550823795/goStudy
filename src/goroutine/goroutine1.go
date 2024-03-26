0463a4703382518b247e3e8a29377a35f8db6676
jy.wang <jy.wang@zuche.com>
2024-03-26 09:32:31 +0800

    Gorountine

---
+++ src/goroutine/goroutine1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func newTask() {
+	i := 0
+	for {
+		i++
+		fmt.Printf("new Gorountine:i=%d\n", i)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	//创建一个go程 去执行newTask()流程
+	go newTask()
+	i := 0
+	for {
+		i++
+		fmt.Printf("main Gorountine:i=%d\n", i)
+		time.Sleep(1 * time.Second)
+	}
+}
