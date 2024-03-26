b7ab9e052edd2a0f1772415a407290836544fb55
jy.wang <jy.wang@zuche.com>
2024-03-26 11:32:50 +0800

    有缓存的channel

--- src/goroutine/channel2.go
+++ src/goroutine/channel2.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+/*
+*
+有缓存的channel
+*/
+func main() {
+	c := make(chan int, 3) //带有缓冲的channel
+	fmt.Println("len(c)=", len(c), ",cap(c)", cap(c))
+
+	go func() {
+		defer fmt.Println("子 goroutine结束")
+		for i := 0; i < 3; i++ {
+			c <- i //send
+			fmt.Println("子go正在运行:,发送的元素:", i, "len(c)=", len(c), ",cap(c)", cap(c))
+		}
+	}()
+	time.Sleep(2 * time.Second)
+	for i := 0; i < 3; i++ {
+		num := <-c //接收
+		fmt.Println("num=", num)
+	}
+	fmt.Println("main goroutine 结束")
 }
